e77bcffae9ec1128914e775972ee772f02d7ebcf
furukawa00000 <furukawa00000@gmail.com>
2014-12-22 14:19:45 -0600

    yasu

--- calibration/align_panorama_to_depth_cli.cc
+++ calibration/align_panorama_to_depth_cli.cc
@@ -430,14 +430,18 @@ void InitializeParameters(const FileIO& file_io, const int p, vector<double>* pa
   // params->at(4) = -ReadOffsetZ(filename) + 60;
 
   // new-breeze
-  params->at(2) = -ReadOffsetTheta(filename) + M_PI * 0.8;
-  params->at(4) = -ReadOffsetZ(filename) - 60;
+  // params->at(2) = -ReadOffsetTheta(filename) + M_PI * 0.8;
+  // params->at(4) = -ReadOffsetZ(filename) - 60;
   // and pyramid_level 2.
 
   // red-lion.
+
+  // salmon palace
   //params->at(2) = -ReadOffsetTheta(filename) + M_PI * 0.8;
   //params->at(4) = -ReadOffsetZ(filename) - 60;
-
+  // salmon palace 19
+  params->at(2) = -ReadOffsetTheta(filename) + M_PI * 0.8;
+  params->at(4) = -ReadOffsetZ(filename) + 60;
   
 
   double dtmp[] = {2.19772, -3.14159e-06, 2.55223, -3.14159e-06, 127.213, -2.36926, 0.632438 };
