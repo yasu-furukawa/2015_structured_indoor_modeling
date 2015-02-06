6a63ee7dea9d4bac50150c7be05caf3e338b5c0b
higerra <higerra@126.com>
2015-02-05 23:42:05 -0600

    Hang

--- object_hole_filling/object_hole_filling.h
+++ object_hole_filling/object_hole_filling.h
@@ -20,22 +20,4 @@ bool visibilityTest(const structured_indoor_modeling::Point &pt, const structure
 
 int groupObject(const structured_indoor_modeling::PointCloud &point_cloud, std::vector <std::vector <int> >& objectgroup);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void getSuperpixelLabel(const structured_indoor_modeling::PointCloud &point_cloud, const structured_indoor_modeling::Panorama &panorama, const std::vector<double> &depthmap, const std::vector<int>& superpixel,const std::vector< std::vector<int> >&labelgroup,  std::vector<int> &superpixelConfidence, int superpixelnum);
--- object_hole_filling/object_hole_filling_cali.cpp
+++ object_hole_filling/object_hole_filling_cali.cpp
@@ -12,6 +12,7 @@
 #include <typeinfo>
 #include "object_hole_filling.h"
 #include "depth_filling.h"
+#include <algorithm>
 
 using namespace std;
 using namespace cv;
@@ -19,7 +20,7 @@ using namespace Eigen;
 using namespace structured_indoor_modeling;
 
 DEFINE_string(config_path,"lumber.configuration","Path to the configuration file");
-DEFINE_int32(label_num,5000,"Number of superpixel");
+DEFINE_int32(label_num,6000,"Number of superpixel");
 
 int main(int argc, char **argv){
 
@@ -63,55 +64,71 @@ int main(int argc, char **argv){
     int numlabels(0);
     vector<int> labels(imgwidth*imgheight);
     char buffer[100];
-#if 0
-    cout<<"Performing SLICO Superpixel..."<<endl;
-    Mat pan = panorama.GetRGBImage().clone();
-    SLIC slic;
-    vector<unsigned int>imagebuffer;
-    MatToImagebuffer(pan, imagebuffer);
-    slic.PerformSLICO_ForGivenK(&imagebuffer[0],imgwidth,imgheight,&labels[0],numlabels,FLAGS_label_num,0.0);
-    slic.DrawContoursAroundSegmentsTwoColors(&imagebuffer[0],&labels[0],imgwidth,imgheight);
 
-    sprintf(buffer,"SLIC%03d.txt",id);
-    slic.SaveSuperpixelLabels(&labels[0],imgwidth,imgheight,numlabels," ",string(buffer));
-    cout<<"numlabels: "<<numlabels<<endl;
-    Mat out;
-    ImagebufferToMat(imagebuffer, imgwidth, imgheight, out);
-    //sprintf(buffer,"%s/SLIC%03d.png",file_io.GetDataDirectory().c_str(),id);
-    sprintf(buffer,"SLIC%03d.png",id); 
-    imwrite(buffer,out);
-    waitKey(10);
-#else
     sprintf(buffer,"SLIC%03d.txt",id);
     ifstream labelin(buffer, ios::binary);
     if(!labelin.is_open()){
-      cout<<"Superpixel file doesn't exist!"<<endl;
-      exit(-1);
+	cout<<"Performing SLICO Superpixel..."<<endl;
+	Mat pan = panorama.GetRGBImage().clone();
+	SLIC slic;
+	vector<unsigned int>imagebuffer;
+	MatToImagebuffer(pan, imagebuffer);
+	slic.PerformSLICO_ForGivenK(&imagebuffer[0],imgwidth,imgheight,&labels[0],numlabels,FLAGS_label_num,0.0);
+	slic.DrawContoursAroundSegmentsTwoColors(&imagebuffer[0],&labels[0],imgwidth,imgheight);
+
+	sprintf(buffer,"SLIC%03d.txt",id);
+	slic.SaveSuperpixelLabels(&labels[0],imgwidth,imgheight,numlabels," ",string(buffer));
+	cout<<"numlabels: "<<numlabels<<endl;
+	Mat out;
+	ImagebufferToMat(imagebuffer, imgwidth, imgheight, out);
+	//sprintf(buffer,"%s/SLIC%03d.png",file_io.GetDataDirectory().c_str(),id);
+	sprintf(buffer,"SLIC%03d.png",id); 
+	imwrite(buffer,out);
+	waitKey(10);
+    }else{
+	labelin.read((char*)&numlabels, sizeof(int));
+	labels.resize(imgwidth * imgheight);
+	for(int i=0;i<labels.size();i++){
+	    labelin.read((char*)&labels[i], sizeof(int));
+	}
+	labelin.close();
     }
-    labelin.read((char*)&numlabels, sizeof(int));
-    labels.resize(imgwidth * imgheight);
-    for(int i=0;i<labels.size();i++){
-      labelin.read((char*)&labels[i], sizeof(int));
-    }
-#endif
     
     //project point_cloud onto panorama
     PointCloud curob;
     cout<<"Reading object ply..."<<endl;
     string objectcloudpath = file_io.GetObjectPointClouds(id);
+    cout << objectcloudpath<<endl;
     curob.Init(objectcloudpath);
-    curob.ToGlobal(file_io, id);
     
     vector <vector<int> >labelgroup;
     labelTolabelgroup(labels, labelgroup, numlabels);
-    vector <bool> pixelLabel(imgwidth * imgheight);
-    vector <bool> superpixelConfidence(numlabels);
+ 
+    vector <int> superpixelConfidence(numlabels);
     vector <vector <int> >objectgroup;
     cout<<"Grouping objects..."<<endl;
     int objectnum = groupObject(curob, objectgroup);
     cout<<"Number of object:"<<objectnum<<endl;
-
     
+    cout<<"Getting superpixel confidence.."<<endl;
+    getSuperpixelLabel(curob, panorama, depth.GetDepthmap(), labels,labelgroup, superpixelConfidence, numlabels);
+#if 1
+    //save the mask
+    int minc = *min_element(superpixelConfidence.begin(), superpixelConfidence.end());
+    int maxc = *max_element(superpixelConfidence.begin(), superpixelConfidence.end());
+
+    Mat outmask = panorama.GetRGBImage().clone();
+    for(int i=0;i<imgwidth*imgheight;++i){
+	int x = i % imgwidth;
+	int y = i / imgwidth;
+	int curconfidence =(int)((float) (superpixelConfidence[labels[i]] - minc) / (float)(maxc - minc) * 255.0);
+	Vec3b curpix((uchar)curconfidence,(uchar)curconfidence,(uchar)curconfidence);
+	outmask.at<Vec3b>(y,x) = curpix;
+    }
+    sprintf(buffer,"objectmask%03d.png",id);
+    imwrite(buffer, outmask);
+    waitKey(10);
+#endif
    
   }
 
