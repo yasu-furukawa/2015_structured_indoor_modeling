fa974b4061d4a8eced38b020d427211f9a5ea9b4
furukawa00000 <furukawa00000@gmail.com>
2015-03-07 19:09:35 -0600

    yasu.

--- texture/generate_texture_indoor_polygon_cli.cc
+++ texture/generate_texture_indoor_polygon_cli.cc
@@ -86,8 +86,7 @@ int main(int argc, char* argv[]) {
   ComputePanoramaDepths(&texture_input);
   
   vector<Patch> patches(texture_input.indoor_polygon.GetNumSegments());
-  // for (int p = 0; p < patches.size(); ++p) {
-  for (int p = 18; p < patches.size(); ++p) {
+  for (int p = 0; p < patches.size(); ++p) {
     cout << p << " segment" << endl;
     const Segment& segment = texture_input.indoor_polygon.GetSegment(p);
     bool visibility_check = false;
