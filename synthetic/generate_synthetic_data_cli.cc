d8d6c5518697de0d6ccd06aa499b6bd971ca0285
furukawa00000 <furukawa00000@gmail.com>
2015-03-10 15:28:47 -0500

    yasu

--- synthetic/generate_synthetic_data_cli.cc
+++ synthetic/generate_synthetic_data_cli.cc
@@ -1,8 +1,27 @@
 #include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <gflags/gflags.h>
+
+#include "../base/file_io.h"
 
 using namespace Eigen;
+using namespace std;
 
-struct CameraParameters {
+DEFINE_int32(width, 512, "Width of a depth panorama.");
+DEFINE_int32(phi_range, 150.0 * M_PI / 180.0, "phi range.");
+
+struct Point {
+  Vector2d uv;
+  Vector3d position;
+  Vector3i color;
+  Vector3d normal;
+  int intensity;
+};
+
+struct Camera {
   Eigen::Vector3d center;
   int width;
   int height;
@@ -10,12 +29,16 @@ struct CameraParameters {
   double phi_range;
 };
 
+struct Mesh {
+  vector<Vector3d> vertices;
+  vector<Vector3i> triangles;
+};
 
-Eigen::Vector2d Project(const CameraParameters& camera_parameters,
+Eigen::Vector2d Project(const Camera& camera,
                         const Eigen::Vector3d& global) {  
-  const Vector3d local = global - center;
+  const Vector3d local = global - camera.center;
   const double phi_per_pixel =
-    camera_parameters.phi_range / camera_parameters.height;
+    camera.phi_range / camera.height;
 
   // x coordinate.
   double theta = -atan2(local.y(), local.x());
@@ -26,51 +49,116 @@ Eigen::Vector2d Project(const CameraParameters& camera_parameters,
     theta_ratio = 0.0;
 
   Vector2d uv;
-  uv[0] = theta_ratio * camera_parameters.width;
+  uv[0] = theta_ratio * camera.width;
   const double depth = sqrt(local.x() * local.x() +
                             local.y() * local.y());
   double phi = atan2(local.z(), depth);
   const double pixel_offset_from_center = phi / phi_per_pixel;
   // uv[1] = height / 2.0 - pixel_offset_from_center;
-  uv[1] = max(0.0, min(camera_parameters.height - 1.1,
-                       camera_parameters.height / 2.0 -
+  uv[1] = max(0.0, min(camera.height - 1.1,
+                       camera.height / 2.0 -
                        pixel_offset_from_center));
 
   return uv;
 }
 
-
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    cerr << "Usage: " << argv[1] << " pano_centers.txt" << endl;
-    exit (1);
-  }
-
+void ReadCameras(const string& filename,
+                 const int width,
+                 const double phi_range,
+                 vector<Camera>* cameras) {
   ifstream ifstr;
-  ifstr.open(argv[1]);
+  ifstr.open(filename.c_str());
   if (!ifstr.is_open()) {
-    cerr << "Cannot open a file." << argv[1] << endl;
+    cerr << "Cannot open a file." << filename << endl;
     exit (1);
   }
-  vector<CameraParameters> camera_parameters;
   while (true) {
-    CameraParameters parameters;
+    Camera camera;
     for (int i = 0; i < 3; ++i)
-      ifstr >> parameters.center[i];
+      ifstr >> camera.center[i];
     if (ifstr.eof())
       break;
 
-    parameters.width  = kWidth;
-    parameters.height = kHeight;
-    parameters.kPhiRange = kPhiRange;
-    camera_parameters.push_back(parameters);
+    camera.width     = width;
+    camera.height    = camera.width / 2;
+    camera.phi_range = phi_range;
+    cameras->push_back(camera);
+  }
+  ifstr.close();
+}
+
+void ReadMesh(const string& filename, Mesh * mesh) {
+  ifstream ifstr;
+  ifstr.open(filename.c_str());
+  string stmp;
+  for (int i = 0; i < 9; ++i)
+    ifstr >> stmp;
+  int num_of_vertices, num_of_faces;
+  ifstr >> num_of_vertices;
+  for (int i = 0; i < 11; ++i)
+    ifstr >> num_of_faces;
+
+  mesh->vertices.resize(num_of_vertices);
+  mesh->triangles.resize(num_of_faces);
+
+  for (int v = 0; v < num_of_vertices; ++v) {
+    for (int i = 0; i < 3; ++i)
+      ifstr >> mesh->vertices[v][i];
+  }
+
+  for (int f = 0; f < num_of_faces; ++f) {
+    int num;
+    ifstr >> num;
+    if (num != 3) {
+      cerr << "Mesh must have only triangles." << endl;
+      exit (1);
+    }
+    for (int i = 0; i < 3; ++i)
+      ifstr >> mesh->triangles[f][i];
+  }
+
+  ifstr.close();
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    cerr << "Usage: " << argv[1] << " data_directory" << endl;
+    exit (1);
+  }
+
+  vector<Camera> cameras;
+  {
+    char buffer[1024];
+    sprintf(buffer, "%s/pano_centers.txt", argv[1]);
+    ReadCameras(buffer, FLAGS_width, FLAGS_phi_range, &cameras);
   }
 
   // Read a 3D model.
-  vector<Vecor3dL> vertices;
-  vectorVector3i> triangles;
-  ReadMesh(
+  vector<Mesh> meshes;
+  {
+    int index = 0;
+    while (true) {
+      char buffer[1024];
+      sprintf(buffer, "%s/model-%03d.ply", argv[1], index);
+      ifstream ifstr;
+      ifstr.open(buffer);
+      if (!ifstr.is_open())
+        break;
+      ifstr.close();
+      
+      Mesh mesh;
+      ReadMesh(buffer, &mesh);
+      meshes.push_back(mesh);
+      ++index;
+    }
+  }
 
+  // Start dumping out.
+  for (int c = 0; c < cameras.size(); ++c) {
+
+  
+  
+  return 0;
 }
 
   
