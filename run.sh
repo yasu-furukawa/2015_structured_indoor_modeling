4810ef397415900d47db50236b2d935df22a0e7b
furukawa00000 <furukawa00000@gmail.com>
2015-03-18 11:03:17 -0500

    yasu

---
+++ run.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+./texture/generate_texture_floorplan_cli $1
+./texture/generate_texture_indoor_polygon_cli $1
+./texture/generate_thumbnail_cli $1
+./object_segmentation/object_segmentation_cli $1
+
