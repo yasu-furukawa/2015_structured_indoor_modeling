c9bf3098d069753d177a9ebad7ff678083a72faa
furukawa00000 <furukawa00000@gmail.com>
2015-03-23 11:50:07 -0500

    yasu

--- post_process/collada/indoor_polygon_to_dae_cli.cc
+++ post_process/collada/indoor_polygon_to_dae_cli.cc
@@ -23,88 +23,188 @@ void SetPolygon(const Segment& segment, const int offset, vector<Polygon>* polyg
   const int num_vertices = segment.vertices.size();
   // For each vertex, in-out vertex pair.
   vector<vector<pair<int, int> > > intervals(num_vertices);
-  for (const auto& triangle : segment.triangles) {
-    for (int index0 = 0; index0 < 3; ++index0) {
-      const int index1 = (index0 + 1) % 3;
-      const int index2 = (index0 + 2) % 3;
-      intervals[triangle.indices[index0]].push_back(make_pair(triangle.indices[index1],
-                                                              triangle.indices[index2]));
+  {
+    for (const auto& triangle : segment.triangles) {
+      for (int index0 = 0; index0 < 3; ++index0) {
+        const int index1 = (index0 + 1) % 3;
+        const int index2 = (index0 + 2) % 3;
+        intervals[triangle.indices[index0]].push_back(make_pair(triangle.indices[index1],
+                                                                triangle.indices[index2]));
+      }
     }
   }
 
   const int kInvalid = -1;
+  const pair<int, int> kInvalidPair(kInvalid, kInvalid);
   // Find concatenated intervals.
   vector<vector<pair<int, int> > > concatenated_intervals(num_vertices);
-  for (int v = 0; v < num_vertices; ++v) {
-    const vector<pair<int, int> >& candidates = intervals[v];
-    vector<bool> used(candidates.size(), false);
-    while (true) {
-      // Pick unused candidate.
-      int first_index = kInvalid;
-      for (int i = 0; i < (int)used.size(); ++i) {
-        if (!used[i]) {
-          first_index = i;
-          break;
-        }
-      }
-
-      if (first_index == kInvalid)
-        break;
-
-      used[first_index] = true;
-
-      pair<int, int> interval = candidates[first_index];
-      // Search forward.
+  {
+    for (int v = 0; v < num_vertices; ++v) {
+      const vector<pair<int, int> >& candidates = intervals[v];
+      vector<bool> used(candidates.size(), false);
       while (true) {
-        // Find an unused candidate, which starts with interval.second.
-        int index = kInvalid;
+        // Pick unused candidate.
+        int first_index = kInvalid;
         for (int i = 0; i < (int)used.size(); ++i) {
-          if (!used[i] && candidates[i].first == interval.second) {
-            index = i;
+          if (!used[i]) {
+            first_index = i;
             break;
           }
         }
-        if (index == kInvalid)
-          break;
-
-        used[index] = true;
-        interval.second = candidates[i].second;
-      }
-      // Search backward.
-      while (true) {
-        // Find an unused candidate, which starts with interval.second.
-        int index = kInvalid;
-        for (int i = 0; i < (int)used.size(); ++i) {
-          if (!used[i] && candidates[i].second == interval.first) {
-            index = i;
-            break;
-          }
-        }
-        if (index == kInvalid)
+        
+        if (first_index == kInvalid)
           break;
         
-        used[index] = true;
-        interval.first = candidates[i].first;
+        used[first_index] = true;
+        
+        pair<int, int> interval = candidates[first_index];
+        // Search forward.
+        while (true) {
+          // Find an unused candidate, which starts with interval.second.
+          int index = kInvalid;
+          for (int i = 0; i < (int)used.size(); ++i) {
+            if (!used[i] && candidates[i].first == interval.second) {
+              index = i;
+              break;
+            }
+          }
+          if (index == kInvalid)
+            break;
+          
+          used[index] = true;
+          interval.second = candidates[index].second;
+        }
+        // Search backward.
+        while (true) {
+          // Find an unused candidate, which starts with interval.second.
+          int index = kInvalid;
+          for (int i = 0; i < (int)used.size(); ++i) {
+            if (!used[i] && candidates[i].second == interval.first) {
+              index = i;
+              break;
+            }
+          }
+          if (index == kInvalid)
+            break;
+          
+          used[index] = true;
+          interval.first = candidates[index].first;
+        }
+        
+        // If interval.first and interval.second are the same, vanish.
+        if (interval.first == interval.second)
+          continue;
+        
+        concatenated_intervals[v].push_back(interval);
       }
-
-      // If interval.first and interval.second are the same, vanish.
-      if (interval.first == interval.second)
-        continue;
-
-      concatenated_intervals[v].push_back(interval);
     }
   }
 
   // Now trace.
+  vector<vector<int> > chains;
   while (true) {
     // Find a vertex that has some concatenated intervals.
+    int first_vertex = kInvalid;
+    pair<int, int> first_interval = kInvalidPair;
+    vector<int> chain;
+    for (int v = 0; v < (int)concatenated_intervals.size(); ++v) {
+      if (first_vertex != kInvalid)
+        break;
+      for (int i = 0; i < (int)concatenated_intervals[v].size(); ++i) {
+        if (concatenated_intervals[v][i].first != kInvalid) {
+          first_vertex = v;
+          first_interval = concatenated_intervals[v][i];
 
+          chain.push_back(v);
+          concatenated_intervals[v][i] = kInvalidPair;
+          break;
+        }
+      }
+    }
 
-    // use offset
+    if (first_vertex == kInvalid)
+      break;
+    
+    // Start from first_vertex.
+    // first_interval.first ---> first_vertex ---> first_interval.second.
 
+    int current_vertex = first_vertex;
+    pair<int, int> current_interval = first_interval;
+    
+    while (true) {
+      const int next_vertex = current_interval.second;
+      pair<int, int> next_interval = kInvalidPair;
+
+      for (int i = 0; i < (int)concatenated_intervals[next_vertex].size(); ++i) {
+        if (concatenated_intervals[next_vertex][i].first == current_vertex) {
+          next_interval = concatenated_intervals[next_vertex][i];
+
+          chain.push_back(next_vertex);
+          concatenated_intervals[next_vertex][i] = kInvalidPair;
+          break;
+        }
+      }
+      if (next_interval == kInvalidPair) {
+        cerr << "Impossible. Cannot come back..." << endl;
+        exit (1);
+      }
+
+      // Cycle detected.
+      if (next_interval.second == first_vertex)
+        break;
+
+      current_vertex = next_vertex;
+      current_interval = next_interval;
+    }
+    chains.push_back(chain);
   }
   
+  // If multiple chains, identify holes.
+  if (chains.empty()) {
+    cerr << "No chain extracted." << endl;
+    exit (1);
+  }
+  
+  if ((int)chains.size() > 1) {
+    // Identify holes.
+    vector<double> volumes(chains.size(), 0);
 
+    for (int c = 0; c < chains.size(); ++c) {
+      Vector3d min_xyz, max_xyz;
+      for (int i = 0; i < (int)chains[c].size(); ++i) {
+        const Vector3d& position = segment.vertices[chains[c][i]];
+        if (i == 0) {
+          min_xyz = max_xyz = position;
+        } else {
+          for (int a = 0; a < 3; ++a) {
+            min_xyz[a] = min(min_xyz[a], position[a]);
+            max_xyz[a] = max(max_xyz[a], position[a]);
+          }
+        }
+      }
+      volumes[c] = (max_xyz[0] - min_xyz[0]) + (max_xyz[1] - min_xyz[1]) + (max_xyz[2] - min_xyz[2]);
+    }
+
+    const int max_index = max_element(volumes.begin(), volumes.end()) - volumes.begin();
+    if (max_index != 0)
+      swap(chains[0], chains[max_index]);
+  }
+
+  // Now chains[0] is the boundary.
+  // Add offset.
+  for (auto& chain : chains) {
+    for (auto& value : chain)
+      value += offset;
+  }
+
+  Polygon polygon;
+  polygon.boundary = chains[0];
+  for (int i = 1; i < (int)chains.size(); ++i) {
+    reverse(chains[i].begin(), chains[i].end());
+    polygon.holes.push_back(chains[i]);
+  }
+
+  polygons->push_back(polygon);
   /*
   set<pair<int, int> > edges;
   for (const auto& triangle : segment.triangles) {
