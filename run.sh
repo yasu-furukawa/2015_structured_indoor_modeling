8d21dfabcf3719ba4b04a8e96f251f4cdd5ca493
furukawa00000 <furukawa00000@gmail.com>
2015-03-24 13:31:13 -0500

    yasu.

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+# Textures and images for viewer.
 ./main_process/texture/generate_texture_floorplan_cli $1
 ./main_process/texture/generate_texture_indoor_polygon_cli $1
 ./main_process/texture/generate_thumbnail_cli $1
+
+# Objects.
 ./main_process/object_segmentation/object_segmentation_cli $1
-# object_hole_filling
-# After deep-learning detection.
+./main_process/object_hole_filling/Object_hole_filling $1
 ./main_process/object_detection/generate_object_icons_cli $1
 
+# Misc.
 ./post_process/collada/indoor_polygon_to_dae_cli $1
 ./post_process/evaluation/evaluate_cli $1
