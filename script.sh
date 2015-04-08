965e206d052027cd592005822a4abefa3c83dfd5
furukawa00000 <furukawa00000@gmail.com>
2015-04-08 10:27:25 -0500

    yasu

---
+++ script.sh
@@ -0,0 +1,17 @@
+#./delete_files.sh ~/Box_Sync/research/structured_indoor_modeling/data/lumber-cashew-1997/
+./run.sh ~/Box_Sync/research/structured_indoor_modeling/data/lumber-cashew-1997/
+
+#./delete_files.sh ~/Box_Sync/research/structured_indoor_modeling/data/equal-sky-0649/
+./run.sh ~/Box_Sync/research/structured_indoor_modeling/data/equal-sky-0649/
+
+#./delete_files.sh ~/Box_Sync/research/structured_indoor_modeling/data/red-lion-9567/
+./run.sh ~/Box_Sync/research/structured_indoor_modeling/data/red-lion-9567/
+
+#./delete_files.sh ~/Box_Sync/research/structured_indoor_modeling/data/salmon-palace-2313/
+./run.sh ~/Box_Sync/research/structured_indoor_modeling/data/salmon-palace-2313/
+
+#./delete_files.sh ~/Box_Sync/research/structured_indoor_modeling/data/new-breeze-6946/
+#./run.sh ~/Box_Sync/research/structured_indoor_modeling/data/new-breeze-6946/
+
+
+
