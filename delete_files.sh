52e45a6e40c58b8a32f622d22d7709e5f6697ba1
furukawa00000 <furukawa00000@gmail.com>
2015-04-10 05:54:55 -0500

    yasu

--- delete_files.sh
+++ delete_files.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 /bin/rm -f $1/evaluation/*
+/bin/rm -f $1/evaluation/images/*
 /bin/rm -f $1/floorplan/*
 /bin/rm -f $1/object/*
 # some imges inside... /bin/rm -f $1/object_detection/*
