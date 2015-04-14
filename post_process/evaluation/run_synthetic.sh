2927de8aedd9dc37b6d718398b252e39e5452e3d
furukawa00000 <furukawa00000@gmail.com>
2015-04-14 04:45:00 -0500

    yasu

---
+++ post_process/evaluation/run_synthetic.sh
@@ -0,0 +1,6 @@
+#./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic0/ --evaluate_all=true
+#./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic1/ --evaluate_all=true
+#./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic2/ --evaluate_all=true
+./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic3/ --evaluate_all=true
+./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic4/ --evaluate_all=true
+./evaluate_cli ~/Box_Sync/research/structured_indoor_modeling/data/synthetic5/ --evaluate_all=true
