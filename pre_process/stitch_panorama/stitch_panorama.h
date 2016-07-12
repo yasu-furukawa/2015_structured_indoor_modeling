62682d47d9bbf4c98ed5f6433ee94d32a9a74fe2
furukawa00000 <furukawa00000@gmail.com>
2016-07-12 04:03:56 -0700

    yasu

--- pre_process/stitch_panorama/stitch_panorama.h
+++ pre_process/stitch_panorama/stitch_panorama.h
@@ -1,6 +1,27 @@
 #ifndef STITCH_PANORAMA_H_
 #define STITCH_PANORAMA_H_
 
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+struct Input {
+  std::vector<Eigen::Matrix3d> intrinsics;
+  std::vector<Eigen::Matrix3d> rotations;
+
+  std::vector<cv::Mat> images;
+};
+
+struct Output {
+  std::vector<Eigen::Matrix3d> refined_intrinsics;
+  std::vector<Eigen::Matrix3d> refined_rotations;
+  cv::Mat blended_image;
+}
 
 
 #endif  // STITCH_PANORAMA_H_
